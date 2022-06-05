40b228e3b52a90f6cdbdace61f8bc541ad41e29c
jiangg494 <63155997+jiangg494@users.noreply.github.com>
2022-06-04 23:53:38 -0400

    Add files via upload

---
+++ Final_Challenge/BDM_Final_gj2065.py
@@ -0,0 +1,143 @@
+from pyspark.sql.session import SparkSession
+from pyspark import Row
+
+import sys
+import json
+from pyspark.sql.functions import col
+from pyspark import SparkFiles
+import math
+
+
+def parse_year_month(date_range_start_flag, date_range_end_flag):
+    flag = ''
+    if (date_range_start_flag == '2019-03' or date_range_end_flag == '2019-03'):
+        flag = '2019-03'
+    elif (date_range_start_flag == '2019-10' or date_range_end_flag == '2019-10'):
+        flag = '2019-10'
+    elif (date_range_start_flag == '2020-03' or date_range_end_flag == '2020-03'):
+        flag = '2020-03'
+    elif (date_range_start_flag == '2020-10' or date_range_end_flag == '2020-10'):
+        flag = '2020-10'
+
+    return flag
+
+
+# haversine distance unit is km
+def haversine2(lonlat1, lonlat2):
+    lat1,lon1 = lonlat1
+    lat2,lon2 = lonlat2
+
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
+    d_lat = lat2 - lat1
+    d_lon = lon2 - lon1
+    a = math.sin(d_lat * 0.5) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon * 0.5) ** 2
+    a = 2 * math.asin(math.sqrt(a)) * 6371
+    return a
+
+
+def parse_vistor_home_cbgs(vistors_home_cbgs, list_nyc_cbg_centroids):
+    dict_cbgs = json.loads(vistors_home_cbgs)
+    cbgs = list(dict_cbgs.keys())
+    if (len(cbgs) == 0):
+        return None
+    # only consider CBG FIPS for NYC
+    cbgs = list(filter(lambda x: x in list_nyc_cbg_centroids, cbgs))
+    if(len(cbgs) == 0):
+        cbgs = None
+    return cbgs
+
+
+def distances(x, dict_cbg_latlong):
+    poi_cbg_latlong = dict_cbg_latlong.get(x[0][0])
+    sum_dis = sum(map(lambda cbg: haversine2(dict_cbg_latlong.get(cbg), poi_cbg_latlong), x[1]))
+    return (x[0], [sum_dis, len(x[1])])
+
+
+def format_month_distance(x):
+    cbg_fips = int(x[0])
+    date_distances = dict(x[1])
+    return Row(cbg_fips, date_distances.get('2019-03', ''), date_distances.get('2019-10', ''),date_distances.get('2020-03', ''), date_distances.get('2020-10', ''))
+
+
+if __name__ == '__main__':
+    output = sys.argv[1]
+    import os
+    nyc_cbg_centroids = os.getenv('SPARK_YARN_STAGING_DIR') + "/nyc_cbg_centroids.csv"
+    nyc_supermarkets = os.getenv('SPARK_YARN_STAGING_DIR') + "/nyc_supermarkets.csv"
+    weekly_patterns = "/tmp/bdm/weekly-patterns-nyc-2019-2020/*"
+    import time
+    start_time = time.time()
+    spark = SparkSession.builder.appName("BDM_Final_gj2065").getOrCreate()
+
+    # 'place_id', 'latitude', 'longitude', 'name', 'vicinity','rating', 'user_ratings_total', 'community_district','percent_food_insecurity', 'safegraph_placekey', 'safegraph_name'
+    nyc_supermarkets_data = spark.read \
+        .option('header', True) \
+        .option('delimiter', ',') \
+        .option('inferSchema', True) \
+        .csv(nyc_supermarkets) \
+        .withColumnRenamed('safegraph_placekey','placekey')\
+        .cache()
+
+    weekly_patterns_data = spark.read \
+        .option('header', True) \
+        .option('delimiter', ',') \
+        .option('inferSchema', False) \
+        .option('escape', '"') \
+        .csv(weekly_patterns) \
+        .select('placekey', 'poi_cbg', 'visitor_home_cbgs', 'date_range_start', 'date_range_end') \
+        .withColumn('date_range_start_flag', col('date_range_start').substr(0, 7)) \
+        .withColumn('date_range_end_flag', col('date_range_end').substr(0, 7))
+
+    #'cbg_fips', 'latitude', 'longitude'
+    nyc_cbg_centroids_data = spark.read \
+        .option('header', True) \
+        .option('delimiter', ',') \
+        .option('inferSchema', True) \
+        .csv(nyc_cbg_centroids) \
+        .cache()
+
+
+    # 1.Use nyc_supermarkets.csv to filter the visits in the weekly patterns data
+    weekly_patterns_data = weekly_patterns_data.join(nyc_supermarkets_data,['placekey']).filter('place_id is not null')
+
+    # 2. Only visit patterns with date_range_start or date_range_end overlaps with the 4
+    # months of interests (Mar 2019, Oct 2019, Mar 2020, Oct 2020) will be considered,
+    # i.e. either the start or the end date falls within the period.
+    weekly_patterns_data = weekly_patterns_data.rdd.map(lambda row: [row['poi_cbg'], row['visitor_home_cbgs'],parse_year_month(row['date_range_start_flag'],row['date_range_end_flag'])]) \
+        .filter(lambda x: len(x[2]) > 0)
+
+    # 3. Use visitor_home_cbgs as the travel origins, and only consider CBG FIPS for NYC (must exist in nyc_cbg_centroids.csv ).
+    list_nyc_cbg_centroids = nyc_cbg_centroids_data.select('cbg_fips') \
+        .rdd \
+        .map(lambda row: str(row[0])) \
+        .collect()
+
+    weekly_patterns_data = weekly_patterns_data.map(lambda row: ((row[0], row[2]), parse_vistor_home_cbgs(row[1], list_nyc_cbg_centroids))) \
+        .filter(lambda x: x[1] != None)
+
+
+    '''
+    4. Travel distances are the distances between each CBG in visitor_home_cbgs with
+    poi_cbg . The distance must be computed in miles. To compute the distance
+    between coordinates locally to NYC more accurately, please project them to the
+    EPSG 2263 first.
+    '''
+    map_cbg_lat_lon = nyc_cbg_centroids_data.rdd \
+        .map(lambda row: (str(row['cbg_fips']), [row['latitude'], row['longitude']])) \
+        .collectAsMap()
+
+    result = spark.createDataFrame(weekly_patterns_data\
+        .map(lambda x: distances(x, map_cbg_lat_lon)) \
+        .reduceByKey(lambda x, y: [ x[0] + y[0], x[1] + y[1] ]) \
+        .map(lambda x: (x[0][0], [x[0][1], round(x[1][0] / x[1][1] * 0.621371, 2)]))\
+        .groupByKey() \
+        .map(lambda group: format_month_distance(group)),['cbg_fips','2019-03','2019-10','2020-03','2020-10']) \
+        .orderBy('cbg_fips')\
+        .write.option('header',True).csv(output)
+
+    end_time = time.time()
+    print('finished,time costs %s s' % ((end_time-start_time)))
